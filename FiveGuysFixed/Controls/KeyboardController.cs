7c7e870575f24fba4a775f9e1d81cacd0c543953
Walid Suleiman <156993022+walidsuleiman@users.noreply.github.com>
2025-02-13 01:48:32 +0000

    Added Controls Back

---
+++ FiveGuysFixed/Controls/KeyboardController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace FiveGuys.Controls
+{
+    public class KeyboardController : IController
+    {
+        private KeyboardState previousState;
+        private KeyboardState currentState;
+        private Game1 game;
+
+        public KeyboardController(Game1 game)
+        {
+            this.game = game;
+            this.currentState = Keyboard.GetState();
+            this.previousState = Keyboard.GetState();
+
+        }
+
+        public void Update()
+        {
+         
+            currentState = Keyboard.GetState();
+
+            if (currentState.IsKeyDown(Keys.W))
+            {
+                //move up
+            }
+            else if (currentState.IsKeyDown(Keys.A))
+            {
+                //move left
+            }
+            else if (currentState.IsKeyDown(Keys.S))
+            {
+                //move down
+            }
+            else if (currentState.IsKeyDown(Keys.D))
+            {
+                //move right
+            }
+            else if (currentState.IsKeyDown(Keys.Space))
+            {
+                //attack
+            }
+           //else if (currentState.IsKeyDown(Keys.Enter))
+           // {
+           //     //pause or menu
+           // }
+            else if (currentState.IsKeyDown(Keys.Q))
+            {
+                Console.WriteLine("Exiting Game");
+                game.Exit();
+            }
+            else if (currentState.IsKeyDown(Keys.R))
+            {
+                //reset or restart
+            }
+            else if (currentState.IsKeyDown(Keys.E))
+            {
+                //interact or equip
+            }
+
+
+            previousState = currentState;
+        }
+
+
+    }
+}
---
+++ FiveGuysFixed/Controls/MouseController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+namespace FiveGuys.Controls
+{
+    public class MouseController : IController
+    {
+        private Game1 game;
+        private MouseState previousState;
+        private MouseState currentState;
+
+        private int windowWidth;
+        private int windowHeight;
+
+        public MouseController(Game1 game)
+        {
+            this.game = game;
+            this.currentState = Mouse.GetState();
+            this.previousState = Mouse.GetState();
+        }
+
+        public void Update()
+        {
+            currentState = Mouse.GetState();
+
+            if (currentState.LeftButton == ButtonState.Pressed)
+            {
+                // Method call to handle left click
+            }
+
+            else if (currentState.RightButton == ButtonState.Pressed)
+            {
+                // Method call to handle right click
+                //This is a test to see if mouse input works:
+                Console.WriteLine("Right Click");
+                game.Exit();
+            }
+
+            previousState = currentState;
+
+        }
+
+
+    }
+}
