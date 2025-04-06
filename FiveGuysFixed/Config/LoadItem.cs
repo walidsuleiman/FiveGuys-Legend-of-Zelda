26806814b5188f2c7b07cf3fe6d0f28517935c9c
Liu Edward <lhp.edward22@gmail.com>
2025-04-06 01:35:40 -0400

    EdwardRefactoring

--- FiveGuysFixed/Config/LoadItem.cs
+++ FiveGuysFixed/Config/LoadItem.cs
@@ -4,12 +4,13 @@ using FiveGuysFixed.Animation;
 
 namespace FiveGuysFixed.Config
 {
+    // stores info for drawing a sprite from a texture
     public class ItemData
     {
         public Texture2D Texture { get; }
         public int X { get; }
         public int Y { get; }
-        public int Dimensions { get; } // This is the width/height of a single frame
+        public int Dimensions { get; }
         public int Frames { get; }
 
         public ItemData(Texture2D texture, int x, int y, int dimensions, int frames)
@@ -22,36 +23,71 @@ namespace FiveGuysFixed.Config
         }
     }
 
+    // loads and organizes enemy and item sprite data
     public class LoadItems
     {
+        // projectiles
         public ItemData bomb;
         public ItemData fireball;
         public ItemData boomerang;
 
+        // enemies
         public ItemData keese;
         public ItemData stalfos;
         public ItemData gel;
+
         public ItemData leftMoblin;
         public ItemData rightMoblin;
         public ItemData upMoblin;
         public ItemData downMoblin;
+
         public ItemData leftGoriya;
         public ItemData rightGoriya;
         public ItemData downGoriya;
         public ItemData upGoriya;
 
+        // boss
         public ItemData Aquamentus;
         public ItemData AquamentusAttack;
 
         public LoadItems(Texture2D texture, Texture2D enemy, Texture2D bosses)
         {
+            InitializeProjectiles(texture);
+            InitializeBasicEnemies(enemy);
+            InitializeDirectionalEnemies(enemy);
+            InitializeBosses(bosses);
+        }
+
+        // creates a new sprite from item data
+        public ISprite getNewItem(ItemData item)
+        {
+            return new Sprite(
+                item.Texture,
+                item.X,
+                item.Y,
+                item.Dimensions,
+                item.Dimensions,
+                item.Frames
+            );
+        }
+
+ 
+        private void InitializeProjectiles(Texture2D texture)
+        {
             fireball = new ItemData(texture, 16, 80, 16, 2);
             boomerang = new ItemData(texture, 16, 48, 16, 3);
+        }
 
-            keese = new ItemData(enemy, 16, 32, 16, 2);    
-            stalfos = new ItemData(enemy, 16, 96, 16, 2);  
+
+        private void InitializeBasicEnemies(Texture2D enemy)
+        {
+            keese = new ItemData(enemy, 16, 32, 16, 2);
+            stalfos = new ItemData(enemy, 16, 96, 16, 2);
             gel = new ItemData(enemy, 16, 0, 16, 2);
+        }
 
+        private void InitializeDirectionalEnemies(Texture2D enemy)
+        {
             leftMoblin = new ItemData(enemy, 80, 112, 16, 2);
             rightMoblin = new ItemData(enemy, 48, 112, 16, 2);
             downMoblin = new ItemData(enemy, 16, 112, 16, 2);
@@ -61,14 +97,13 @@ namespace FiveGuysFixed.Config
             rightGoriya = new ItemData(enemy, 48, 48, 16, 2);
             downGoriya = new ItemData(enemy, 16, 48, 16, 2);
             upGoriya = new ItemData(enemy, 112, 48, 16, 2);
-
-            Aquamentus = new ItemData(bosses, 64, 0, 32, 4);     
-            AquamentusAttack = new ItemData(bosses, 0, 0, 32, 2);
         }
 
-        public ISprite getNewItem(ItemData item)
+
+        private void InitializeBosses(Texture2D bosses)
         {
-            return new Sprite(item.Texture, item.X, item.Y, item.Dimensions, item.Dimensions, item.Frames);
+            Aquamentus = new ItemData(bosses, 64, 0, 32, 4);
+            AquamentusAttack = new ItemData(bosses, 0, 0, 32, 2);
         }
     }
 }
--- FiveGuysFixed/LinkPlayer/PlayerState.cs
+++ FiveGuysFixed/LinkPlayer/PlayerState.cs
@@ -1,46 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FiveGuysFixed.Common;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+using FiveGuysFixed.Common;
 using FiveGuysFixed.Items;
 
 namespace FiveGuysFixed.LinkPlayer
 {
-
-
+    // tracks player state like position, health, inventory, etc.
     public class PlayerState
     {
         public Vector2 position;
         public bool isMoving;
-        public WeaponType heldWeapon;
         public Dir direction;
+        public Vector2 transitionOffset;
+
+        public WeaponType heldWeapon;
         public bool isAttacking;
         public int health;
+
         public int greenRupees;
         public int redRupees;
         public int bombCount;
         public int foodCount;
 
-        public Vector2 transitionOffset;
-
-        public PlayerState(Vector2 position) 
+        public PlayerState(Vector2 position)
         {
-            this.position = position;
-            this.isMoving = false;
-            this.isAttacking = false;
-            this.heldWeapon = WeaponType.NONE;
-            this.direction = Dir.DOWN;
-            this.health = 6;
-            this.greenRupees = 0;
-            this.redRupees = 0;
+            InitializeMovementState(position);
+            InitializeCombatState();
+            InitializeInventory();
+        }
 
-            this.transitionOffset = Vector2.Zero;
-            this.bombCount = 0;
-            this.foodCount = 0;
+        private void InitializeMovementState(Vector2 startPosition)
+        {
+            this.position = startPosition;
+            isMoving = false;
+            direction = Dir.DOWN;
+            transitionOffset = Vector2.Zero;
+        }
+
+        private void InitializeCombatState()
+        {
+            isAttacking = false;
+            heldWeapon = WeaponType.NONE;
+            health = 6; // max health = 6
+        }
+
+        private void InitializeInventory()
+        {
+            greenRupees = 0;
+            redRupees = 0;
+            bombCount = 0;
+            foodCount = 0;
         }
     }
 }
