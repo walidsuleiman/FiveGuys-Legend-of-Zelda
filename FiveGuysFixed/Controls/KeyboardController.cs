7f04340ad5d5b1836dd567014ac8200e91cc95c1
walidsuleiman <suleiman.43@osu.edu>
2025-04-29 15:20:02 -0400

    Gamepad and Keyboard Controller Refactored

--- FiveGuysFixed/Controls/KeyboardController.cs
+++ FiveGuysFixed/Controls/KeyboardController.cs
@@ -1,21 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
-using FiveGuys.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using FiveGuysFixed.Animation;
 using FiveGuysFixed.Commands;
 using FiveGuysFixed.Common;
 using FiveGuysFixed.Config;
 using FiveGuysFixed.GameStates;
-using FiveGuysFixed.LinkPlayer;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
+using FiveGuys.Controls;
+using System.Linq;
 
 namespace FiveGuysFixed.Controls
 {
@@ -36,135 +31,31 @@ namespace FiveGuysFixed.Controls
         {
             currentState = Keyboard.GetState();
 
-            // Array of keys we care about.
+            HandleMovementKeys();
+            HandleCombatKeys();
+            HandleItemKeys();
+            HandleEnemyKeys();
+            HandleGameKeys();
+            HandleAudioKeys();
+            HandleBoomerangKeys();
+            HandleBlockKeys();
+            HandleDifficultyToggle();
+
+            previousState = currentState;
+        }
+
+        private void HandleMovementKeys()
+        {
             Keys[] movementKeys = { Keys.W, Keys.A, Keys.S, Keys.D, Keys.Up, Keys.Down, Keys.Left, Keys.Right };
-            Keys[] itemKeys = { Keys.U, Keys.I, Keys.B };
-            Keys[] weaponKeys = { Keys.D1, Keys.D2, Keys.D3 };
-            Keys[] enemyKeys = { Keys.O, Keys.P };
-            Keys[] gameKeys = { Keys.Q, Keys.R, Keys.Enter, Keys.C, Keys.L, Keys.X };
-            Keys[] blockKeys = { Keys.T, Keys.Y };
-            Keys[] combatKeys = { Keys.OemComma, Keys.E };
-            Keys[] audioKeys = { Keys.M };
-            Keys[] boomerangKeys = { Keys.OemPeriod };
 
-
-            //// Boomerang key handling
-            //foreach (Keys bKey in boomerangKeys)
-            //{
-            //    bool currentDown = currentState.IsKeyDown(bKey);
-            //    bool previousDown = previousState.IsKeyDown(bKey);
-
-            //    // Check for key press (not release)
-            //    if (currentDown && !previousDown)
-            //    {
-            //        // Call boomerang specific method
-            //        ThrowBoomerang();
-            //    }
-            //}
-
-            foreach (Keys cKey in combatKeys)
-            {
-                bool currentDown = currentState.IsKeyDown(cKey);
-                bool previousDown = previousState.IsKeyDown(cKey);
-
-                // Determine if the key was just pressed or just released.
-                if (!currentDown && previousDown)
-                {
-                    bool isKeyDown = currentDown;
-
-                    switch (cKey)
-                    {
-                        case Keys.OemComma:
-                            // Only use for sword attack
-                            if (GameState.PlayerState.heldWeapon == WeaponType.WOODSWORD ||
-                                GameState.PlayerState.heldWeapon == WeaponType.WHITESWORD)
-                            {
-                                GameState.Player.Attack();
-                            }
-                            break;
-                        case Keys.E:
-                            //GameState.Player.TakeDamage(1);//Not Needed anymore, was used for sprint2
-                            break;
-                    }
-                }
-            }
-
-
-            if (currentState.IsKeyDown(Keys.F1) && !previousState.IsKeyDown(Keys.F1))
-            {
-                // Cycle through difficulties for testing
-                if (DifficultyManager.Instance.CurrentDifficulty == GameDifficulty.Easy)
-                {
-                    DifficultyManager.Instance.SetDifficulty(GameDifficulty.Hard);
-                    System.Diagnostics.Debug.WriteLine("Difficulty set to: Hard");
-                }
-                else if (DifficultyManager.Instance.CurrentDifficulty == GameDifficulty.Hard)
-                {
-                    DifficultyManager.Instance.SetDifficulty(GameDifficulty.Hell);
-                    System.Diagnostics.Debug.WriteLine("Difficulty set to: Hell");
-                }
-                else
-                {
-                    DifficultyManager.Instance.SetDifficulty(GameDifficulty.Easy);
-                    System.Diagnostics.Debug.WriteLine("Difficulty set to: Easy");
-                }
-            }
-            foreach (Keys gKey in gameKeys)
-            {
-                if (currentState.IsKeyDown(gKey) && !previousState.IsKeyDown(gKey))
-                {
-                    switch (gKey)
-                    {
-                        case Keys.Q:
-                            game.Exit();
-                            break;
-                        case Keys.R:
-                            GameState.Player.Reset();
-                            break;
-                        case Keys.Enter:
-                            if (GameStateManager.CurrentState is GamePlayState)
-                                GameStateManager.SetState(new PauseState(game));
-                            else if (GameStateManager.CurrentState is PauseState)
-                                GameStateManager.SetState(new GamePlayState(game));
-                            break;
-                        case Keys.C:
-                            if (GameStateManager.CurrentState is GamePlayState)
-                            {
-                                GameStateManager.SetState(new Inventory(game));
-                            }
-                            else if (GameStateManager.CurrentState is Inventory)
-                            {
-                                GameStateManager.SetState(new GamePlayState(game));
-                            }
-                            break;
-                        case Keys.X:
-                            if (GameStateManager.CurrentState is GamePlayState)
-                            {
-                                GameStateManager.SetState(new ShopState(game));
-                            }
-                            else if (GameStateManager.CurrentState is ShopState)
-                            {
-                                GameStateManager.SetState(new GamePlayState(game));
-                            }
-                            break;
-                        case Keys.L:
-                            GameState.darkMode = !GameState.darkMode;
-                            Debug.WriteLine("Dark mode toggled: " + GameState.darkMode);
-                            break;
-                    }
-                }
-            }
-
-            // Process movement keys using a switch statement.
             foreach (Keys mKey in movementKeys)
             {
                 bool currentDown = currentState.IsKeyDown(mKey);
                 bool previousDown = previousState.IsKeyDown(mKey);
 
-                // Determine if the key was just pressed or just released.
-                if (currentDown && !previousDown || !currentDown && previousDown)
+                if (currentDown != previousDown)
                 {
-                    bool isKeyDown = currentDown; // true if just pressed, false if just released
+                    bool isKeyDown = currentDown;
                     switch (mKey)
                     {
                         case Keys.W:
@@ -186,28 +77,37 @@ namespace FiveGuysFixed.Controls
                     }
                 }
             }
+        }
 
-            foreach (Keys eKey in enemyKeys)
+        private void HandleCombatKeys()
+        {
+            Keys[] combatKeys = { Keys.OemComma, Keys.E };
+            foreach (Keys cKey in combatKeys)
             {
-                bool currentDown = currentState.IsKeyDown(eKey);
-                bool previousDown = previousState.IsKeyDown(eKey);
+                bool currentDown = currentState.IsKeyDown(cKey);
+                bool previousDown = previousState.IsKeyDown(cKey);
 
-                // Determine if the key was just pressed or just released.
                 if (!currentDown && previousDown)
                 {
-                    bool isKeyDown = currentDown; // true if just pressed, false if just released
-                    switch (eKey)
+                    switch (cKey)
                     {
-                        case Keys.P:
-                            //new EnemySwitchCommand(game, true).Execute();//Not Needed anymore, was used for sprint2
+                        case Keys.OemComma:
+                            if (GameState.PlayerState.heldWeapon == WeaponType.WOODSWORD ||
+                                GameState.PlayerState.heldWeapon == WeaponType.WHITESWORD)
+                            {
+                                GameState.Player.Attack();
+                            }
                             break;
-                        case Keys.O:
-                            //new EnemySwitchCommand(game, false).Execute();//Not Needed anymore, was used for sprint2
+                        case Keys.E:
                             break;
                     }
                 }
             }
+        }
 
+        private void HandleItemKeys()
+        {
+            Keys[] itemKeys = { Keys.U, Keys.I, Keys.B };
             foreach (Keys iKey in itemKeys)
             {
                 bool currentDown = currentState.IsKeyDown(iKey);
@@ -215,15 +115,8 @@ namespace FiveGuysFixed.Controls
 
                 if (!currentDown && previousDown)
                 {
-                    bool isKeyDown = currentDown;
                     switch (iKey)
                     {
-                        case Keys.U:
-                            //new ItemSwitchCommand(game, true).Execute();//Not Needed anymore, was used for sprint2
-                            break;
-                        case Keys.I:
-                            //new ItemSwitchCommand(game, false).Execute();//Not Needed anymore, was used for sprint2
-                            break;
                         case Keys.B:
                             var slot = GameState.EquippedB;
                             if (slot == null) break;
@@ -239,13 +132,12 @@ namespace FiveGuysFixed.Controls
                                 case "Bomb":
                                     GameState.PendingBomb = true;
                                     GameState.PendingPos = GameState.PlayerState.position;
-                                    GameState.PlayerState.health--; // cost
+                                    GameState.PlayerState.health--;
                                     break;
                                 case "Food":
                                     GameState.PlayerState.health++;
                                     break;
                             }
-
                             count--;
                             slot.SetCount(count);
                             if (count <= 0) GameState.EquippedB = null;
@@ -253,7 +145,90 @@ namespace FiveGuysFixed.Controls
                     }
                 }
             }
+        }
 
+        private void HandleEnemyKeys()
+        {
+            Keys[] enemyKeys = { Keys.O, Keys.P };
+            foreach (Keys eKey in enemyKeys)
+            {
+                bool currentDown = currentState.IsKeyDown(eKey);
+                bool previousDown = previousState.IsKeyDown(eKey);
+
+                if (!currentDown && previousDown)
+                {
+                    // Old sprint2 feature commented out
+                }
+            }
+        }
+
+        private void HandleGameKeys()
+        {
+            Keys[] gameKeys = { Keys.Q, Keys.R, Keys.Enter, Keys.C, Keys.L, Keys.X };
+            foreach (Keys gKey in gameKeys)
+            {
+                if (currentState.IsKeyDown(gKey) && !previousState.IsKeyDown(gKey))
+                {
+                    switch (gKey)
+                    {
+                        case Keys.Q:
+                            game.Exit();
+                            break;
+                        case Keys.R:
+                            GameState.Player.Reset();
+                            break;
+                        case Keys.Enter:
+                            if (GameStateManager.CurrentState is GamePlayState)
+                                GameStateManager.SetState(new PauseState(game));
+                            else if (GameStateManager.CurrentState is PauseState)
+                                GameStateManager.SetState(new GamePlayState(game));
+                            break;
+                        case Keys.C:
+                            if (GameStateManager.CurrentState is GamePlayState)
+                                GameStateManager.SetState(new Inventory(game));
+                            else if (GameStateManager.CurrentState is Inventory)
+                                GameStateManager.SetState(new GamePlayState(game));
+                            break;
+                        case Keys.X:
+                            if (GameStateManager.CurrentState is GamePlayState)
+                                GameStateManager.SetState(new ShopState(game));
+                            else if (GameStateManager.CurrentState is ShopState)
+                                GameStateManager.SetState(new GamePlayState(game));
+                            break;
+                        case Keys.L:
+                            GameState.darkMode = !GameState.darkMode;
+                            Debug.WriteLine("Dark mode toggled: " + GameState.darkMode);
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void HandleAudioKeys()
+        {
+            if (currentState.IsKeyDown(Keys.M) && !previousState.IsKeyDown(Keys.M))
+            {
+                game.isMuted = !game.isMuted;
+                MediaPlayer.Volume = game.isMuted ? 0f : 0.5f;
+
+                if (!game.isMuted && MediaPlayer.State != MediaState.Playing)
+                {
+                    MediaPlayer.Play(game.backgroundMusic);
+                }
+            }
+        }
+
+        private void HandleBoomerangKeys()
+        {
+            if (currentState.IsKeyDown(Keys.OemPeriod) && !previousState.IsKeyDown(Keys.OemPeriod))
+            {
+                ThrowBoomerang();
+            }
+        }
+
+        private void HandleBlockKeys()
+        {
+            Keys[] blockKeys = { Keys.T, Keys.Y };
             foreach (Keys bKey in blockKeys)
             {
                 bool currentDown = currentState.IsKeyDown(bKey);
@@ -261,69 +236,34 @@ namespace FiveGuysFixed.Controls
 
                 if (!currentDown && previousDown)
                 {
-                    bool isKeyDown = currentDown;
-                    switch (bKey)
-                    {
-                        case Keys.T:
-                            //new BlockSwitchCommand(game, true).Execute();//Not Needed anymore, was used for sprint2
-                            break;
-                        case Keys.Y:
-                            //new BlockSwitchCommand(game, false).Execute();//Not Needed anymore, was used for sprint2
-                            break;
-                    }
+                    // Old sprint2 feature commented out
                 }
             }
+        }
 
-            foreach (Keys audioKey in audioKeys)
+        private void HandleDifficultyToggle()
+        {
+            if (currentState.IsKeyDown(Keys.F1) && !previousState.IsKeyDown(Keys.F1))
             {
-                bool currentDown = currentState.IsKeyDown(audioKey);
-                bool previousDown = previousState.IsKeyDown(audioKey);
-
-                if (currentDown && !previousDown)
+                var dm = DifficultyManager.Instance;
+                if (dm.CurrentDifficulty == GameDifficulty.Easy)
                 {
-                    switch (audioKey)
-                    {
-                        case Keys.M:
-                            game.isMuted = !game.isMuted;
-                            if (game.isMuted)
-                            {
-                                //mute
-                                MediaPlayer.Volume = 0f;
-                            }
-                            else
-                            {
-                                MediaPlayer.Volume = 0.5f;
-                                if (MediaPlayer.State != MediaState.Playing)
-                                {
-                                    MediaPlayer.Play(game.backgroundMusic);
-                                }
-                            }
-                            break;
-                    }
+                    dm.SetDifficulty(GameDifficulty.Hard);
                 }
-            }
-
-            foreach (Keys boomerangKey in boomerangKeys)
-            {
-                bool currentDown = currentState.IsKeyDown(boomerangKey);
-                bool previousDown = previousState.IsKeyDown(boomerangKey);
-                if (currentDown && !previousDown)
+                else if (dm.CurrentDifficulty == GameDifficulty.Hard)
                 {
-                    switch (boomerangKey)
-                    {
-                        case Keys.OemPeriod:
-                            ThrowBoomerang();
-                            break;
-                    }
+                    dm.SetDifficulty(GameDifficulty.Hell);
                 }
+                else
+                {
+                    dm.SetDifficulty(GameDifficulty.Easy);
+                }
+                Debug.WriteLine("Difficulty set to: " + dm.CurrentDifficulty);
             }
-
-            previousState = currentState;
         }
 
         private void ThrowBoomerang()
         {
-            // Create boomerang velocity based on direction
             Vector2 velocity = Vector2.Zero;
             Vector2 startPos = GameState.PlayerState.position;
 
@@ -347,37 +287,20 @@ namespace FiveGuysFixed.Controls
                     break;
             }
 
-            // Check if there's already a boomerang
-            bool boomerangExists = false;
-            foreach (var projectile in GameState.roomManager.getCurrentRoom().Projectiles)
-            {
-                if (projectile is FiveGuysFixed.Projectiles.Boomerang)
-                {
-                    boomerangExists = true;
-                    break;
-                }
-            }
+            bool boomerangExists = GameState.roomManager.getCurrentRoom().Projectiles
+                .Any(p => p is FiveGuysFixed.Projectiles.Boomerang);
 
-            // Only throw if no boomerang exists
             if (!boomerangExists)
             {
-                // Create and add boomerang
                 try
                 {
                     Texture2D weaponTexture = game.Content.Load<Texture2D>("linkSprite");
                     GameState.roomManager.getCurrentRoom().Projectiles.Add(
-                        new FiveGuysFixed.Projectiles.Boomerang(
-                            weaponTexture,
-                            startPos.X,
-                            startPos.Y,
-                            velocity
-                        )
-                    );
+                        new FiveGuysFixed.Projectiles.Boomerang(weaponTexture, startPos.X, startPos.Y, velocity));
                 }
                 catch (Exception ex)
                 {
-                    // Handle exception (texture not found, etc.)
-                    System.Diagnostics.Debug.WriteLine("Boomerang creation error: " + ex.Message);
+                    Debug.WriteLine("Boomerang creation error: " + ex.Message);
                 }
             }
         }
